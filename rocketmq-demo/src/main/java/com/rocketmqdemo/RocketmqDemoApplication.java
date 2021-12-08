c00856a05dec9db680fb6e13ab645446686fc84c
luojiyuan <893049558@qq.com>
2021-12-08 15:38:45 +0800

    first commit

---
+++ rocketmq-demo/src/main/java/com/rocketmqdemo/RocketmqDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rocketmqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqDemoApplication.class, args);
+    }
+
+}
