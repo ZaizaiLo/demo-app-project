7e954eff684eaac9e48ad354a07897a09184e06b
luojiyuan <893049558@qq.com>
2021-12-14 16:14:48 +0800

    second commit

--- rocketmq-demo/src/main/java/com/rocketmqdemo/RocketmqDemoApplication.java
+++ rocketmq-demo/src/main/java/com/rocketmqdemo/RocketmqDemoApplication.java
@@ -3,6 +3,9 @@ package com.rocketmqdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author ZaiZai
+ */
 @SpringBootApplication
 public class RocketmqDemoApplication {
 
---
+++ rocketmq-demo/src/main/java/com/rocketmqdemo/config/Test.java
@@ -0,0 +1,9 @@
+package com.rocketmqdemo.config;
+
+/**
+ * @description
+ * @author luojiyuan
+ * @date 2021-12-08 16:26
+ */
+public class Test {
+}
