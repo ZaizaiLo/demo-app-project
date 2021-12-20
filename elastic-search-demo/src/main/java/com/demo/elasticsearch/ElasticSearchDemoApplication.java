4493d05e5aa78343e6ff3bf56295049c2e07d0ab
luojiyuan <893049558@qq.com>
2021-12-20 10:41:35 +0800

    feature:中间件demo集合     添加ES

--- elastic-search-demo/src/main/java/com/demo/elasticsearch/ElasticSearchDemoApplication.java
+++ elastic-search-demo/src/main/java/com/demo/elasticsearch/ElasticSearchDemoApplication.java
@@ -3,6 +3,9 @@ package com.demo.elasticsearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author ZaiZai
+ */
 @SpringBootApplication
 public class ElasticSearchDemoApplication {
 
